9e867d0db91773103a32a00f914b27e9b172e8cc
Buğrahan <bsaygici@hotmail.com>
2022-02-07 23:48:16 +0300

    Added work on ListTypes in Redis to Project

---
+++ RedisWeb/Controllers/ListTypeController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RedisWeb.Services;
+using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RedisWeb.Controllers
+{
+    public class ListTypeController : Controller
+    {
+        private readonly RedisService _redisService;
+        private readonly IDatabase db;
+        private string key = "names";
+        public ListTypeController(RedisService redisService)
+        {
+            _redisService = redisService;
+            db = _redisService.GetDb(0);
+        }
+        public IActionResult Index()
+        {
+            List<string> nameList = new();
+            if (db.KeyExists(key))
+            {
+                db.ListRange(key).ToList().ForEach(x =>
+                {
+                    nameList.Add(x.ToString());
+                });
+
+            }
+            return View(nameList);
+        }
+        [HttpPost]
+        public IActionResult Add(string name)
+        {
+            if(name is not null)
+            {
+                db.ListRightPushAsync(key, name);
+            }
+            return RedirectToAction("Index");
+        }
+        public IActionResult Delete(string name)
+        {
+            db.ListRemoveAsync(key, name).Wait();
+            return RedirectToAction("Index");
+        }
+    }
+}
