b4b679b8019a7e305b31ce6089b03928ea4bc67f
buğrahan saygıcı <bsaygici@hotmail.com>
2022-02-08 17:18:31 +0300

    Added work on HashTypes in Redis to Project

---
+++ RedisWeb/Controllers/BaseController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RedisWeb.Services;
+using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RedisWeb.Controllers
+{
+    public class BaseController : Controller
+    {
+        private readonly RedisService _redisService;
+        protected readonly IDatabase db;
+        public BaseController(RedisService redisService)
+        {
+            _redisService = redisService;
+            db = _redisService.GetDb(0);
+        }
+    }
+}
---
+++ RedisWeb/Controllers/HashTypeController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RedisWeb.Services;
+using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RedisWeb.Controllers
+{
+    public class HashTypeController : BaseController
+    {
+        public string hashKey { get; set; } = "sozluk";
+        public HashTypeController(RedisService redisService) : base(redisService)
+        {
+        }
+
+        public IActionResult Index()
+        {
+            Dictionary<string, string> list = new Dictionary<string, string>();
+            if(db.KeyExists(hashKey))
+            {
+                var aa = db.HashGetAll(hashKey).ToList();
+                foreach (var item in aa)
+                {
+                    list.Add(item.Name, item.Value);
+                }
+            }
+            return View(list);
+        }
+        [HttpPost]
+        public IActionResult Add(string name, string value)
+        {
+            db.HashSet(hashKey, name, value);
+            return RedirectToAction("Index");
+        }
+        public IActionResult Delete(string name)
+        {
+            db.HashDelete(hashKey, name);
+            return RedirectToAction("Index");
+        }
+    }
+}
